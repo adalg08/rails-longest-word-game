deab3080c2dfc3a4c05b035827ba74b6087b8414
Adalberto GONZALEZ <adalg08@gmail.com>
2017-05-08 18:03:44 +0200

    backend finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'game', to: 'turn#game'
+  get 'score', to: 'turn#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
